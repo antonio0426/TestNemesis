67e01ce9169a40116aafb93708515736c7e3359a
ant.mangano0426 <ant.mangano@gmail.com>
2022-06-20 11:29:08 +0200

    Initial commit

---
+++ src/main/java/com/hpe/nemesis/flyway/RunFlyway.java
@@ -0,0 +1,40 @@
+package com.hpe.nemesis.flyway;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Observes;
+import javax.inject.Inject;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.flywaydb.core.Flyway;
+import org.jboss.logging.Logger;
+import io.quarkus.runtime.StartupEvent;
+
+@ApplicationScoped
+public class RunFlyway {
+
+	@Inject
+	Logger log;
+
+	@ConfigProperty(name = "myapp.flyway.migrate")
+	boolean runMigration;
+
+	@ConfigProperty(name = "quarkus.datasource.jdbc.url")
+	String datasourceUrl;
+	@ConfigProperty(name = "quarkus.datasource.username")
+	String datasourceUsername;
+	@ConfigProperty(name = "quarkus.datasource.password")
+	String datasourcePassword;
+
+	public void runFlywayMigration(@Observes StartupEvent event, StartUpTest startUpTest, StartUpTest2 startUpTest2,
+			StartUpTest3 startUpTest3) {
+		log.info("############################runFlywayMigration############################");
+		if (runMigration) {
+			Flyway flyway = Flyway.configure()
+					.dataSource("jdbc:" + datasourceUrl, datasourceUsername, datasourcePassword).load();
+			flyway.migrate();
+		}
+		startUpTest.runTest();
+		startUpTest2.runTest();
+		startUpTest3.runTest();
+	}
+}
---
+++ src/main/java/com/hpe/nemesis/flyway/StartUpTest3.java
@@ -0,0 +1,21 @@
+package com.hpe.nemesis.flyway;
+
+import javax.enterprise.context.ApplicationScoped;
+ 
+import javax.inject.Inject;
+ 
+import org.jboss.logging.Logger;
+
+ 
+
+@ApplicationScoped
+public class StartUpTest3 {
+
+	@Inject
+	Logger log;
+
+	public void runTest() {
+		log.info("############################runTest3############################");
+
+	}
+}
